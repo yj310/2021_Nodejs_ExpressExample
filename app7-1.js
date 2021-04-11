ac684e3bda4da4c26d3e7e13daa76e06c68e9e26
lyj310 <yoenjii310@gmail.com>
2021-04-11 23:33:11 +0900

    21.04.09
    npm install path --save
    npm install body-parser --save

---
+++ app7-1.js
@@ -0,0 +1,48 @@
+// Express 기본 모듈 불러오기
+var express = require('express')
+    , http = require('http')
+    , path = require('path');
+
+// Express의 미들웨어 불러오기
+var bodyParser = require('body-parser')
+    , static = require('serve-static');
+
+// 익스프레스 객체 생성
+var app = express();
+
+
+
+// 기본 속성 설정
+app.set('port', process.env.PORT || 3000);
+
+// body-parser를 이용해 application/x-www-form-urlencoded 파싱
+app.use(bodyParser.urlencoded({extended: false}));
+
+// body-parser를 이용해 application/json 파싱
+app.use(bodyParser.json());
+
+app.use('/', static(path.join(__dirname, 'public')));
+
+// 미들웨어에서 파라미터 확인
+app.use(function(req, res, next) {
+    console.log('첫 번째 미들웨어에서 요청을 처리함.');
+    
+    res.redirect('/');
+});
+
+
+
+
+// Express 서버 시작
+http.createServer(app).listen(3000, function(){
+    console.log('Express 서버가 3000번 포트에서 시작됨.');
+});
+
+
+
+/*
+    http://localhost:3000
+    http://localhost:3000/image/house.jpg
+    http://localhost:3000/stylesheets/style.css
+    http://localhost:3000/login.html
+*/
